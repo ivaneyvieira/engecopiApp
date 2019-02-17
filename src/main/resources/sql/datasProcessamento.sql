0b599e0dbca60c3d8b7e157a1a04dcb1aa5bc86b
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2019-02-17 15:04:38 -0300

    atualiza

--- src/main/resources/sql/datasProcessamento.sql
+++ src/main/resources/sql/datasProcessamento.sql
@@ -1,10 +1,8 @@
-select YEAR(current_date)*10000 + 0101 as dataInicial,
-       MAX(date) as dataFinal
-FROM(
-    select date, count(*) as quant
-    FROM sqldados.stkchk
-    where ym = MID(current_date*1, 1, 6)*1
-        and storeno in (1, 2, 3, 4, 5, 6, 7, 10)
-    GROUP BY date
-    HAVING quant > 10000
-    ) AS D
+select YEAR(current_date)*10000 + 0101 as dataInicial, MAX(date) as dataFinal
+FROM (
+       select date as date
+       FROM sqldados.stkchk
+       where ym = MID(current_date*1, 1, 6)*1
+           and storeno in (1, 2, 3, 4, 5, 6, 7, 10)
+       LIMIT 1000
+     ) AS D
