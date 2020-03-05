d89b873e6e8234e548b7a9f60ca0d38c7deac83e
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2020-03-05 07:35:03 -0300

    Grava nota de garantia

--- src/main/kotlin/br/com/engecopi/saci/QuerySaci.kt
+++ src/main/kotlin/br/com/engecopi/saci/QuerySaci.kt
@@ -25,7 +25,7 @@ class QuerySaci: QueryDB(driver, url, username, password) {
   fun pedidoNota(storeno: Int, numero: String): Pedido? {
     val sql = "/sql/pedido.sql"
     return query(sql) {q ->
-      q.addParameter("storeno", storeno)
+      q.addParameter("storeno", storeno) A
         .addParameter("ordno", numero)
         .executeAndFetchFirst(Pedido::class.java)
     }
