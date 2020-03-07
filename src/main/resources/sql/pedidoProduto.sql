4b753f1f29edbb7e99e161675dcc35d9270326bb
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2020-03-07 09:41:11 -0300

    bug_itens_sem_localizacao

--- src/main/resources/sql/pedidoProduto.sql
+++ src/main/resources/sql/pedidoProduto.sql
@@ -1,7 +1,8 @@
 SELECT E.storeno AS storeno, E.ordno AS numero, E.prdno AS prdno, E.grade AS grade,
        qtty / 1000 AS quant, IF(I.last_cost = 0, I.cm_varejo_otn, I.last_cost) / 10000 AS preco,
        TRIM(MID(P.name, 1, 37)) AS descricao,
-       GROUP_CONCAT(DISTINCT localizacao ORDER BY localizacao SEPARATOR '/') AS localizacao
+       IFNULL(GROUP_CONCAT(DISTINCT localizacao ORDER BY localizacao SEPARATOR '/'),
+              '') AS localizacao
 FROM sqldados.eoprd          AS E
   INNER JOIN sqldados.eord      O
                USING (storeno, ordno)
@@ -10,9 +11,8 @@ FROM sqldados.eoprd          AS E
   INNER JOIN sqldados.stk    AS I
                ON I.storeno = E.storeno AND I.prdno = E.prdno AND I.grade = E.grade
   LEFT JOIN  sqldados.prdloc AS L
-               ON E.storeno = L.storeno AND E.prdno = L.prdno
-WHERE O.status IN (1, 4) AND O.storeno = :storeno AND O.ordno = :numero AND :serie = '' AND
-      L.localizacao <> 'CD00'
+               ON E.storeno = L.storeno AND E.prdno = L.prdno AND L.localizacao <> 'CD00'
+WHERE O.status IN (1, 4) AND O.storeno = :storeno AND O.ordno = :numero AND :serie = ''
 GROUP BY storeno, ordno, prdno, grade
 UNION
 DISTINCT
