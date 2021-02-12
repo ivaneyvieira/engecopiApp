61427c919abb444cc5a23646842d50c609b5bff7
Ivaney Vieira <ivaneyvieira@gmail.com>
2021-02-12 06:12:06 -0300

    Error

--- src/main/kotlin/br/com/engecopi/app/EngecopiUI.kt
+++ src/main/kotlin/br/com/engecopi/app/EngecopiUI.kt
@@ -19,43 +19,36 @@ import com.vaadin.ui.Notification
 import com.vaadin.ui.UI
 import com.vaadin.ui.VerticalLayout
 import com.vaadin.ui.themes.ValoTheme
-import org.slf4j.LoggerFactory
 
 @Theme("mytheme")
 @Title("Engecopi App")
-@Push(value = PushMode.MANUAL,  transport = Transport.WEBSOCKET_XHR)
-class EngecopiUI : UI() {
-
+@Push(value = PushMode.MANUAL, transport = Transport.WEBSOCKET_XHR)
+class EngecopiUI: UI() {
   private val content = Content()
-
+  
   override fun init(request: VaadinRequest?) {
     page.setTitle("Engecopi App: " + QuerySaci.ipServer)
     setContent(content)
     navigator = Navigator(this, content as ViewDisplay)
     navigator.addProvider(autoViewProvider)
-    setErrorHandler { e ->
-      log.error("Vaadin UI uncaught exception ${e.throwable}", e.throwable)
-      // when the exception occurs, show a nice notification
-      Notification("Oops", "An error occurred, and we are really sorry about that. Already working on the fix!", Notification.Type.ERROR_MESSAGE).apply {
+    setErrorHandler {
+      Notification("Oops",
+                   "An error occurred, and we are really sorry about that. Already working on the fix!",
+                   Notification.Type.ERROR_MESSAGE).apply {
         styleName = "${ValoTheme.NOTIFICATION_CLOSABLE} ${ValoTheme.NOTIFICATION_ERROR}"
         position = Position.TOP_CENTER
         show(Page.getCurrent())
       }
     }
   }
-
-  companion object {
-    @JvmStatic
-    private val log = LoggerFactory.getLogger(EngecopiUI::class.java)
-  }
 }
 
-private class Content : VerticalLayout(), ViewDisplay {
+private class Content: VerticalLayout(), ViewDisplay {
   init {
     setSizeFull()
     isMargin = false
   }
-
+  
   override fun showView(view: View?) {
     removeAllComponents()
     addComponent(view as Component)
