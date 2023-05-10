d0ef9a58deb821a7254f3df41335e41e4d15e95d
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2023-05-10 20:44:00 -0300

    Adição de novas colunas no painel de movimentação

--- src/main/kotlin/br/com/engecopi/saci/beans/PedidoProduto.kt
+++ src/main/kotlin/br/com/engecopi/saci/beans/PedidoProduto.kt
@@ -1,10 +1,16 @@
 package br.com.engecopi.saci.beans
 
-class PedidoProduto(val storeno: Int?,
-                    val numero: String?,
-                    val prdno: String?,
-                    val grade: String?,
-                    val quant: Double?,
-                    val preco: Double?,
-                    val descricao: String?,
-                    val localizacao: String?)
+class PedidoProduto(
+        val storeno: Int?,
+        val numero: String?,
+        val prdno: String?,
+        val grade: String?,
+        val quant: Double?,
+        val preco: Double?,
+        val descricao: String?,
+        val localizacao: String?,
+        val cl: Int?,
+        val estoque: Double?,
+        val fornecedor: Int?,
+        val tipo: Int?,
+)
--- src/main/resources/sql/pedidoProduto.sql
+++ src/main/resources/sql/pedidoProduto.sql
@@ -1,21 +1,21 @@
-SELECT E.storeno                                                                         AS storeno,
-       E.ordno                                                                           AS numero,
-       E.prdno                                                                           AS prdno,
-       E.grade                                                                           AS grade,
-       qtty / 1000                                                                       AS quant,
-       IF(I.last_cost = 0, I.cm_varejo_otn, I.last_cost) / 10000                         AS preco,
-       TRIM(MID(P.name, 1, 37))                                                          AS descricao,
+SELECT E.storeno                                                 AS storeno,
+       E.ordno                                                   AS numero,
+       E.prdno                                                   AS prdno,
+       E.grade                                                   AS grade,
+       qtty / 1000                                               AS quant,
+       IF(I.last_cost = 0, I.cm_varejo_otn, I.last_cost) / 10000 AS preco,
+       TRIM(MID(P.name, 1, 37))                                  AS descricao,
        IFNULL(GROUP_CONCAT(DISTINCT localizacao ORDER BY localizacao SEPARATOR '/'),
-	      '')                                                                        AS localizacao
-FROM sqldados.eoprd          AS E
-  INNER JOIN sqldados.eord      O
-	       USING (storeno, ordno)
-  INNER JOIN sqldados.prd    AS P
-	       ON E.prdno = P.no
-  INNER JOIN sqldados.stk    AS I
-	       ON I.storeno = E.storeno AND I.prdno = E.prdno AND I.grade = E.grade
-  LEFT JOIN  sqldados.prdloc AS L
-	       ON E.storeno = L.storeno AND E.prdno = L.prdno AND L.localizacao <> 'CD00'
+              '')                                                AS localizacao
+FROM sqldados.eoprd AS E
+         INNER JOIN sqldados.eord O
+                    USING (storeno, ordno)
+         INNER JOIN sqldados.prd AS P
+                    ON E.prdno = P.no
+         INNER JOIN sqldados.stk AS I
+                    ON I.storeno = E.storeno AND I.prdno = E.prdno AND I.grade = E.grade
+         LEFT JOIN sqldados.prdloc AS L
+                   ON E.storeno = L.storeno AND E.prdno = L.prdno AND L.localizacao <> 'CD00'
 WHERE O.status IN (1, 4)
   AND O.storeno = :storeno
   AND O.ordno = :numero
@@ -30,16 +30,20 @@ SELECT X.storeno                                                             AS 
        ROUND(qtty, 3)                                                        AS quant,
        IF(S.last_cost = 0, S.cm_varejo_otn, S.last_cost) / 10000             AS preco,
        TRIM(MID(P.name, 1, 37))                                              AS descricao,
-       GROUP_CONCAT(DISTINCT localizacao ORDER BY localizacao SEPARATOR '/') AS localizacao
-FROM sqldados.xaprd          AS X
-  INNER JOIN sqldados.nf     AS N
-	       USING (storeno, pdvno, xano)
-  INNER JOIN sqldados.prd    AS P
-	       ON X.prdno = P.no
-  INNER JOIN sqldados.stk    AS S
-	       ON S.storeno = X.storeno AND S.prdno = X.prdno AND S.grade = X.grade
-  LEFT JOIN  sqldados.prdloc AS L
-	       ON X.storeno = L.storeno AND X.prdno = L.prdno AND L.localizacao <> 'CD00'
+       GROUP_CONCAT(DISTINCT localizacao ORDER BY localizacao SEPARATOR '/') AS localizacao,
+       P.clno                                                                as cl,
+       S.qtty_varejo / 1000                                                  as estoque,
+       P.mfno                                                                as fornecedor,
+       P.typeno                                                              as tipo
+FROM sqldados.xaprd AS X
+         INNER JOIN sqldados.nf AS N
+                    USING (storeno, pdvno, xano)
+         INNER JOIN sqldados.prd AS P
+                    ON X.prdno = P.no
+         INNER JOIN sqldados.stk AS S
+                    ON S.storeno = X.storeno AND S.prdno = X.prdno AND S.grade = X.grade
+         LEFT JOIN sqldados.prdloc AS L
+                   ON X.storeno = L.storeno AND X.prdno = L.prdno AND L.localizacao <> 'CD00'
 WHERE N.tipo = 2
   AND N.storeno = :storeno
   AND N.nfno = :numero
