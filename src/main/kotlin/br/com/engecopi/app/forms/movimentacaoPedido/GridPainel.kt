0298013944de36fde3ddb5de6ffe081dc9f53c45
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2023-05-18 11:17:37 -0300

    Totalizador no rodape do grid

--- src/main/kotlin/br/com/engecopi/app/forms/movimentacaoPedido/GridPainel.kt
+++ src/main/kotlin/br/com/engecopi/app/forms/movimentacaoPedido/GridPainel.kt
@@ -1,17 +1,20 @@
 package br.com.engecopi.app.forms.movimentacaoPedido
 
 import br.com.engecopi.saci.beans.PedidoProduto
+import br.com.engecopi.utils.format
 import com.github.mvysny.karibudsl.v8.column
 import com.github.mvysny.karibudsl.v8.grid
 import com.github.mvysny.karibudsl.v8.panel
 import com.github.mvysny.karibudsl.v8.showColumns
 import com.vaadin.data.provider.ListDataProvider
 import com.vaadin.ui.CssLayout
+import com.vaadin.ui.components.grid.FooterCell
 import com.vaadin.ui.renderers.NumberRenderer
 import com.vaadin.ui.themes.ValoTheme.LAYOUT_WELL
 import java.text.DecimalFormat
 
 class GridPainel : CssLayout() {
+  private var totalFotter: FooterCell? = null
   val grid = grid(PedidoProduto::class, null, ListDataProvider(emptyList())) {
     setSizeFull()
 
@@ -86,6 +89,17 @@ class GridPainel : CssLayout() {
       PedidoProduto::preco,
       PedidoProduto::total,
     )
+
+    totalFotter = this.appendFooterRow().getCell("total").apply {
+      text = ""
+      styleName = "v-align-right"
+    }
+  }
+
+  fun setItens(itens: List<PedidoProduto>) {
+    grid.dataProvider = ListDataProvider(itens)
+    val total = itens.sumByDouble { it.total ?: 0.0 }
+    totalFotter?.html = "<font size=\"4\"><b>Total:</b> ${total.format()}</font>"
   }
 
   init {
