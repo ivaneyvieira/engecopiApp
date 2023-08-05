6f56f7735c1434e807c75322b76a32aeabef2605
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2023-08-05 11:53:37 -0300

    Volta ao processamento comleto dos itens

--- src/main/resources/sql/ajustaInventarioMov.sql
+++ src/main/resources/sql/ajustaInventarioMov.sql
@@ -21,6 +21,45 @@ SELECT @XANO                          AS xano,
        CONCAT('AJUSTE ', @TIPO, @XANO) AS remarks
 FROM DUAL;
 
+INSERT INTO sqldados.stkmovh (xano, qtty, date, nfno, cm_fiscal, cm_real, auxLong1, auxLong2,
+                              auxLong3, auxLong4, auxLong5, auxMy1, auxMy2, auxMy3, auxMy4, auxMy5,
+                              storeno, userno, tipo, bits, auxShort1, auxShort2, auxShort3,
+                              auxShort4, auxShort5, prdno, grade, nfse, auxStr1, auxStr2, auxStr3,
+                              auxStr4)
+SELECT @XANO  AS xano,
+       @QTTD  AS qtty,
+       @DATA  AS date,
+       0      AS nfno,
+       @CUSTO AS cm_fiscal,
+       @CUSTO AS cm_real,
+       0      AS auxLong1,
+       0      AS auxLong2,
+       0      AS auxLong3,
+       0      AS auxLong4,
+       0      AS auxLong5,
+       0      AS auxMy1,
+       0      AS auxMy2,
+       0      AS auxMy3,
+       0      AS auxMy4,
+       0      AS auxMy5,
+       @LOJA  AS storeno,
+       1      AS userno,
+       0      AS tipo,
+       0      AS bits,
+       0      AS auxShort1,
+       0      AS auxShort2,
+       0      AS auxShort3,
+       0      AS auxShort4,
+       0      AS auxShort5,
+       @PRDNO AS prdno,
+       @GRADE AS grade,
+       0      AS nfse,
+       0      AS auxStr1,
+       0      AS auxStr2,
+       0      AS auxStr3,
+       0      AS auxStr4
+FROM DUAL;
+
 UPDATE sqldados.stk
 SET stk.qtty_atacado = (stk.qtty_atacado + @QTTD)
 WHERE (stk.storeno = @LOJA)
